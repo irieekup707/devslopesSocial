a4e049447923a485bd62389177ab408992318c31
Ryan  Martino <ryanmartino@Ryans-MacBook-Pro.local>
2017-06-07 22:18:48 -0700

    FeedUI part1 complete

---
+++ devslopesSocial/CircleView.swift
@@ -0,0 +1,22 @@
+//
+//  CircleView.swift
+//  devslopesSocial
+//
+//  Created by Ryan  Martino on 6/7/17.
+//  Copyright © 2017 Ryan  Martino. All rights reserved.
+//
+
+import UIKit
+
+class CircleView: UIImageView {
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+
+    override func layoutSubviews() {
+        
+        layer.cornerRadius = self.frame.width / 2
+        
+    }
+}
