51217481cc37d0271c3d2c53a501d5f68513e4e3
Ryan  Martino <ryanmartino@Ryans-MacBook-Pro.local>
2017-06-08 19:05:44 -0700

    UIImagePickerComplete

--- devslopesSocial/FeedVC.swift
+++ devslopesSocial/FeedVC.swift
@@ -10,17 +10,22 @@ import UIKit
 import Firebase
 import SwiftKeychainWrapper
 
-class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     var posts = [Post]()
+    var imagePicker: UIImagePickerController!
     
     @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var imageAdd: CircleView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         tableView.delegate = self
         tableView.dataSource = self
+        imagePicker = UIImagePickerController()
+        imagePicker.allowsEditing = true
+        imagePicker.delegate = self
         
         DataService.ds.REF_POST.observe(.value, with: { (snapshot) in
             if let snapshot = snapshot.children.allObjects as? [DataSnapshot] {
@@ -55,8 +60,24 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
             return cell
         }else {
             return PostCell()
+            }
         }
-    }
+        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let image = info[UIImagePickerControllerEditedImage] as? UIImage {
+            imageAdd.image = image
+        }else {
+            print("Ryan: A Valid Image Wasnt Selected")
+        }
+        imagePicker.dismiss(animated: true, completion: nil)
+        }
+
+    
+   
+    
+        @IBAction func addImageTapped(_ sender: Any) {
+            present(imagePicker, animated: true, completion: nil)
+        }
+        
     
     
     @IBAction func signOutTapped(_ sender: Any) {
